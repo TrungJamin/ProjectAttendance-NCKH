e44273cfbd2dbbd7b6e9d668241cdc905ec812a7
Phuoc <ripker1805@gmail.com>
2021-03-21 22:19:18 +0700

    update version2

---
+++ public/index.js
@@ -0,0 +1,34 @@
+const containerLogin = document.querySelector(".containerLogin");
+const login = document.getElementsByClassName("login");
+
+login[0].addEventListener("submit", (event) => {
+  event.preventDefault();
+  var email = login[0]["email"].value;
+  var password = login[0]["password"].value;
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .then(async function (response) {
+      await db
+        .collection("admin")
+        .get()
+
+        .then(function (querySnapshot) {
+          let check = false;
+          querySnapshot.forEach(function (doc) {
+            if (doc.id === response.user.uid) {
+              check = true;
+            }
+          });
+          return check; // can suy nghi
+        })
+        .then((res) => {
+          res
+            ? location.assign("./Admin/admin.html")
+            : location.assign("./Teacher/HompageTeacher.html");
+        });
+    })
+    .catch(function (error) {
+      alert(error.massages);
+    });
+});
