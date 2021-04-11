8798d51a82a36f436d80256ffec8e527ab2ac8ef
Ho-Thanh <79647705+Ho-Thanh@users.noreply.github.com>
2021-04-11 20:45:50 +0700

    giao dien chua reponsive

--- public/Teacher/index.js
+++ public/Teacher/index.js
@@ -2,4 +2,4 @@ const showStudents = document.querySelector("#list");
 const iFrame = document.querySelector(".iFrame");
 iFrame.src = "./ListStudentLeader.html";
 const day = document.getElementsByClassName('#day');
-
+const view = document.getElementsByClassName('#view');
