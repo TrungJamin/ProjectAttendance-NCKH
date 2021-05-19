b77d8ea043b2c02737641707aed62cf112604fdd
Ho-Thanh <79647705+Ho-Thanh@users.noreply.github.com>
2021-05-19 23:03:08 +0700

    link thang

--- public/Teacher/js/amonth.js
+++ public/Teacher/js/amonth.js
@@ -24,13 +24,16 @@ const monthNow = document.querySelector(".month-now");
 const tableMonth = document.querySelector(".table-list-month");
 
 const nextmonth = document.querySelector(".next-month");
-const backmonth = document.querySelector(".back-month");
+const backmonth = document.querySelector(".previous-month");
+const nowMonth = document.querySelector(".now-month");
 
+let pre = 0;
+let next = 0;
 function MonthNow() {
   let d = new Date();
   return (
     " Month :" +
-    (d.getMonth() + 1) +
+    (d.getMonth() + 1 + next - pre) +
     "/" +
     d.getFullYear()
   );
@@ -42,37 +45,67 @@ let countAbsent = 0;
 function listMonth(listStudents, aMonth) {
   
   let listMonth = [];
-  console.log(listStudents);
+
   listStudents.forEach((student) => {
     let att = student.attendance.filter((atd)=>{
       
       let month = atd.day.split('-');
-      console.log(month+"   "+aMonth);
+     
        return month[0] == aMonth;
     });
-  
+
     listMonth.push({
       ...student,
       attendance: att,
     });
   });
+
   return listMonth;
 }
 function renderMonth(listStudents) {
   let d  = new Date();
- let list = listMonth(listStudents,d.getMonth()+1 );
+  countHAbsent = 0;
+ countAbsent = 0;
+ let list = listMonth(listStudents,d.getMonth()+1 + next - pre);
   let table = list.map((student, index) => {
-    
+    student.attendance.forEach((att)=>{
+     
+      if(!att.data.status){
+        att.data.asked? countHAbsent ++ : countAbsent++;
+      }
+    })
+    console.log(countHAbsent);
+    console.log(countAbsent);
     return `
     <tr>
     <td>${index + 1}</td>
     <td>${student.id}</td>
     <td>${student.firstName}</td>
     <td>${student.lastName}</td>
-    <td>${3}</td>
-    <td>${3}</td>
+    <td>${countHAbsent}</td>
+    <td>${countAbsent}</td>
 </tr>
     `;
   });
   tableMonth.innerHTML = table.join(" ");
 };
+
+nextmonth.addEventListener("click",(e)=>{
+  e.preventDefault();
+  next++;
+  monthNow.innerText = MonthNow();
+  renderMonth(listStudents);
+});
+backmonth.addEventListener("click",(e)=>{
+  e.preventDefault();
+  pre++;
+  monthNow.innerText = MonthNow();
+  renderMonth(listStudents);
+});
+nowMonth.addEventListener("click",(e)=>{
+  e.preventDefault();
+  pre = 0;
+  next = 0;
+  monthNow.innerText = MonthNow();
+  renderMonth(listStudents);
+});
