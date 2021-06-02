2dd2a8f2d3bff6b91105dd36e1340b887c2bdbb1
Phuoc <ripker1805@gmail.com>
2021-06-02 17:09:27 +0700

    add webcam

--- public/test/js/testapi.js
+++ public/test/js/testapi.js
@@ -3,37 +3,41 @@ const isFaceStudent = firebase
   .functions()
   .httpsCallable("detectedListAttendance");
 const addImage = firebase.functions().httpsCallable("addDescriptorsInData");
+const formAdd = document.querySelector("#results");
+const save = document.querySelector(".save");
+console.log(save);
+let listBase64 = [];
+let index = 0;
+save.addEventListener("click", (e) => {
+  e.preventDefault();
 
-const imgUpload = document.getElementById("imageUpload");
-imgUpload.addEventListener("change", async () => {
-  const toBase64 = async (file) =>
-    new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject(error);
-    });
-  toBase64(imgUpload.files[0]).then((result) => {
-    console.log(result);
-    console.log("loading...");
-    // isFaceStudent({
-    //   img: result,
-    //   class: "10A1",
-    // })
-    //   .then((res) => {
-    //     console.log("success");
-    //     console.log(res);
-    //   })
-    //   .catch((err) => {
-    //     console.log(err.message);
-    //   });
-
+  Promise.all(listBase64).then((values) => {
+    console.log("loading image");
     addImage({
-      listBase64: [result],
-      id: "201223",
+      id: "10001",
       Class: "10A1",
-    }).then((res) => {
-      console.log(res);
+      listBase64: values,
+    }).then((values) => {
+      console.log(values);
     });
   });
 });
+Webcam.set({
+  width: 240,
+  height: 240,
+  image_format: "jpeg",
+  jpeg_quality: 90,
+});
+Webcam.attach("#my_camera");
+
+function take_snapshot() {
+  // take snapshot and get image data
+  Webcam.snap(function (data_uri) {
+    // display results in page
+
+    document
+      .getElementById("results")
+      .insertAdjacentHTML("beforeend", `<img id=${index} src="${data_uri}"/>`);
+    listBase64 = [...listBase64, data_uri];
+  });
+}
