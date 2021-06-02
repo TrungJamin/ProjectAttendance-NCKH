88ba0ee3d16032b19d01e9106e5ee009d5689b5c
Phuoc <ripker1805@gmail.com>
2021-06-02 09:12:06 +0700

    change forlder add Face

--- public/Admin/addFaceDataBase/index.js
+++ public/Admin/addFaceDataBase/index.js
@@ -1,3 +1,8 @@
-var myDropzone = new Dropzone("div#myId", {
-  url: "https://attendance-aae4c-default-rtdb.asia-southeast1.firebasedatabase.app/facesDatabase",
+const functions = firebase.functions();
+let testAddFace = functions.httpsCallable("detectedListAttendance");
+
+testAddFace({
+  id: "562001",
+  Class: "10A1",
+  base64: [],
 });
