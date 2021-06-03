ff144cafd7917ae58dc25176b6f1f565d47cef57
cin12211 <56920718+cin12211@users.noreply.github.com>
2021-06-03 22:26:05 +0700

    done function face student witdh id and class

--- public/Admin/addFaceDataBase/index.js
+++ public/Admin/addFaceDataBase/index.js
@@ -1,4 +1,3 @@
-console.log("chay");
 const isFaceStudent = firebase.functions().httpsCallable("getListAttendance");
 const addImage = firebase.functions().httpsCallable("addDescriptorsInData");
 const formAdd = document.querySelector("#results");
@@ -6,20 +5,43 @@ const save = document.querySelector(".save");
 let listBase64 = [];
 let index = 0;
 
+var valueClass='';
+var idStudent='';
+
 save.addEventListener("click", (e) => {
   e.preventDefault();
-
-  Promise.all(listBase64).then((values) => {
+  
+  if(valueClass!=''&&idStudent!=''){
+    Promise.all(listBase64).then((values) => {
     console.log("loading image");
     console.log(values);
     addImage({
-      id: "10001",
-      Class: "10A1",
+      id: idStudent,
+      Class: valueClass,
       listBase64: values,
     }).then((values) => {
+
+      // up lên thanh công reset form 
+      listBase64 = [];
+      var listImg = document.querySelector("#results");
+      listImg.innerHTML='';
+      valueClass='';
+      idStudent='';
+      var selectionClassLeader = document.querySelector("#classLeader");
+      var selectionStudent = document.querySelector("#chooseStudent");
+      selectionClassLeader.value='';
+      selectionStudent.value='';
+
+
       console.log(values);
     });
   });
+  }
+  else{
+
+    alert("chọn lơp và id học sinh")
+  }
+  
 });
 Webcam.set({
   width: 400,
@@ -64,4 +86,88 @@ function removeImg(id) {
 
 
 
-// con chuc nang mai add img lên data base mai làm 
+
+// render selectionClassLeader lop hoc
+
+var selectionClassLeader = document.querySelector("#classLeader");
+
+function getListClass() {
+
+  db.collection("Classes")
+    .get()
+    .then(function (querySnapshot) {
+      var arr = [];
+      querySnapshot.forEach(function (doc) {
+        var tamp = doc.data().classes;
+        var gan = [];
+        tamp.forEach((e) => {
+          gan.push(e);
+        });
+        arr.push(gan);
+        
+      });
+
+
+      renderChooseClassLeader(arr);
+       
+      renderMuntilChoose(arr);
+    })
+    .catch(function (error) { console.log( error)});
+}
+
+getListClass();
+
+//renderChooseClassLeader
+function renderChooseClassLeader(arr) {
+  var node = `<option value checked > chọn class</option>`;
+  arr.forEach((e) => {
+    e.forEach((eClass) => {
+      node += `<option value='${eClass}' > chủ nhiệm lớp ${eClass}</option>`;
+    });
+  });
+  selectionClassLeader.innerHTML = node;
+}
+
+
+
+function renderChooseStudent(arr){
+
+  var selectionStudent = document.querySelector("#chooseStudent");
+
+  var nodeDataStudent='<option checked >chọn học sinh</option>';
+
+  arr.forEach(e=>{
+    nodeDataStudent+=`<option value='${e.id}' > ${e.id} - ${e.lastName}</option>`;
+  })
+
+  selectionStudent.innerHTML=nodeDataStudent;
+}
+
+
+function onChangeClass(className){
+  // test query student 
+
+  valueClass=className;
+  const dataStudent=[];
+ db
+    .collection("Students").where("class", "==", className)
+    .get()
+    .then(function (querySnapshot) {
+        querySnapshot.forEach(function (doc) {
+          
+          dataStudent.push(doc.data());
+         
+        });
+        renderChooseStudent(dataStudent)
+    })
+    .catch(function (error) {
+
+    })
+ 
+}
+
+function onChangeStudent(id){
+  // test query student 
+  idStudent=id;
+}
+
