333042ad608e66c5c843ee2d3f1d1a492d57221e
Phuoc <ripker1805@gmail.com>
2021-06-20 09:40:58 +0700

    list class

--- public/Teacher/js/attendance/index.js
+++ public/Teacher/js/attendance/index.js
@@ -3,13 +3,39 @@
 // Tham Khảo code RenderStudents của aDay.js rồi render ra theo ngày
 // Nhìn cách hoạt động của moblie rồi bắt đầu code
 
+var ListClassTeaches = [];
+
+function getClassByClass(uid) {
+  db.collection("Students")
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {});
+    })
+    .catch(function (error) {});
+}
+
+const DOM_Classes = document.querySelector(".classes");
+const renderListClassTeach = (classes) => {
+  DOM_Classes.innerHTML = classes
+    .map(
+      (
+        teach
+      ) => `   <button class="btn btn-success classes-teach" type="button">
+  <i class="fas fa-chalkboard-teacher"></i>
+  ${teach.class}  ${teach.subject}
+</button>`
+    )
+    .join(" ");
+};
 firebase.auth().onAuthStateChanged((user) => {
+  ListClassTeaches = [];
   if (user.uid) {
     db.collection("Teachers")
       .doc(user.uid)
       .get()
       .then(function (doc) {
-        console.log(doc.data());
+        ListClassTeaches = [...doc.data().subjectsAndClass];
+        renderListClassTeach(ListClassTeaches);
       })
       .catch(function (error) {});
   }
