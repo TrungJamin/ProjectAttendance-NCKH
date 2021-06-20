9d0af47823af6f1b540accd7ed4de96ae94658f3
Phuoc <ripker1805@gmail.com>
2021-06-20 09:08:13 +0700

    .

--- public/Teacher/js/attendance/index.js
+++ public/Teacher/js/attendance/index.js
@@ -2,3 +2,15 @@
 // Rồi từ lớp dạy hiển thị danh sách học sinh
 // Tham Khảo code RenderStudents của aDay.js rồi render ra theo ngày
 // Nhìn cách hoạt động của moblie rồi bắt đầu code
+
+firebase.auth().onAuthStateChanged((user) => {
+  if (user.uid) {
+    db.collection("Teachers")
+      .doc(user.uid)
+      .get()
+      .then(function (doc) {
+        console.log(doc.data());
+      })
+      .catch(function (error) {});
+  }
+});
